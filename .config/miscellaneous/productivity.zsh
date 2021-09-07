8d9e6561d35b118624516d325bf6b92c9e743256
jessekelighine <jessekelighine@gmail.com>
2021-09-08 01:19:39 +0800

    productivity and brew list

---
+++ .config/miscellaneous/productivity.zsh
@@ -0,0 +1,49 @@
+#!/usr/bin/zsh
+
+HOSTS=/etc/hosts
+UNBLOCKED=$(grep '^# # PRODUCTIVITY {{{$' $HOSTS)
+MESS_1=" * Shutting down time wasting websites. Sei produktiv!"
+BROWSER=Safari
+COMMAND1='call search("^\\(# \\)\\{1,2}PRODUCTIVITY {{{$", "e") | exe "norm '
+COMMAND2='" | x'
+
+function toggle_n_kill() {
+	if [ -z $UNBLOCKED ]; then
+		NORM='V%0o0\<C-V>I# \<Esc>'
+	else
+		NORM='V%0o0\<C-V>lx'
+	fi
+	sudo vim +${COMMAND1}$NORM${COMMAND2} $HOSTS
+	killall Safari 2> /dev/null
+	sleep 1
+	open -a $BROWSER
+}
+
+if [[ ! -z $1 ]]; then
+	if [[ -z $UNBLOCKED ]]; then
+		echo "Will unblock for "$1" seconds... zzz"
+		toggle_n_kill
+		sleep $1
+		echo $MESS_1
+		toggle_n_kill
+	fi
+	exit 0
+fi
+
+if [ -z $UNBLOCKED ]; then
+	echo ""
+	echo "You are about to UNBLOCK some time wasting websites!"
+	echo "If you just want to take a break, you could..."
+	echo "\t- listen to a podcast"
+	echo "\t- read some German."
+	echo "\t- check your TODO list."
+	echo "Think about your future before you toggle this..."
+	echo ""
+else
+	echo "\n"$MESS_1"\n"
+fi
+
+if read -q 'SURE?Press Y/y to confirm : '; then
+	echo ''
+	toggle_n_kill
+fi
